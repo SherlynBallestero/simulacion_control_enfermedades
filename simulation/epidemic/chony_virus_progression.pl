33f93a1e7d9eb47c488162315ebe6231dca486d2
chonyy <mlchony7@gmail.com>
2024-04-10 17:48:04 -0400

    fix bug prolog

--- simulation/epidemic/chony_virus_progression.pl
+++ simulation/epidemic/chony_virus_progression.pl
@@ -11,50 +11,54 @@
 % age_group(none, none).
 
 symptoms(1, []).
-stage(1, 'asymptomatic').
-age_group(1, 'old').
+stage(1, symptomatic).
+age_group(1, old).
 
 
 possible_symptoms_symptomatic([normal_fever, normal_cough, normal_short_breath, back_ache, stomach_ache, lazyness, sleepiness]).
 possible_symptoms_critical([critical_fever, critical_cough, critical_short_breath, gastritis]).
 possible_symptoms_terminal([candela, que_ostine]).
 
-advance_dissease('terminal', AgeGroup, Symptoms, NextStage):-
+advance_dissease(terminal, AgeGroup, Symptoms, NextStage):-
     random(X),
-    (X < 0.5 -> NextStage = 'dead';
-    NextStage = 'terminal').
+    (X < 0.5 -> NextStage = dead;
+    NextStage = terminal).
 
-advance_dissease('critical', AgeGroup, Symptoms, NextStage):-
+advance_dissease(critical, AgeGroup, Symptoms, NextStage):-
     length(Symptoms, Length),
     random(X),
     possible_symptoms_critical(Possible_Symptoms),
-    (Length < 2 -> 
-        random_member(Symptom, Possible_Symptoms),
+    (
+    Length < 2 -> 
+        (random_member(Symptom, Possible_Symptoms),
         append(Symptoms, [Symptom], NewSymptoms),
-        NextStage = 'critical';
+        NextStage = critical);
     Length >= 2 -> 
-        (X < 0.5 -> NextStage = 'terminal';
+        (X < 0.5 -> NextStage = terminal;
         random_member(Symptom, Possible_Symptoms),
         not(member(Symptom, Symptoms)),
         append(Symptoms, [Symptom], NewSymptoms),
-        NextStage = 'critical';
-        NextStage = 'recovered')).
+        NextStage = critical;
+        NextStage = recovered)
+    ).
 
 advance_dissease(symptomatic, AgeGroup, Symptoms, NextStage):-
     length(Symptoms, Length),
     random(X),
     possible_symptoms_symptomatic(Possible_Symptoms),
-    (Length < 2 -> 
-        random_member(Symptom, Possible_Symptoms),
+    (
+    Length < 2 -> 
+        (random_member(Symptom, Possible_Symptoms),
         append(Symptoms, [Symptom], NewSymptoms),
-        NextStage = symptomatic;
+        NextStage = symptomatic);
     Length >= 2 ->
         (X < 0.5 -> NextStage = critical;
         random_member(Symptom, Possible_Symptoms),
         not(member(Symptom, Symptoms)),
         append(Symptoms, [Symptom], NewSymptoms),
         NextStage = symptomatic;
-        NextStage = recovered)).
+        NextStage = recovered)
+    ).
 
 advance_dissease_asymptomatic(asymptomatic, AgeGroup, NextStage):-
     random(X),
@@ -68,8 +72,9 @@ step(ID, NextStage):-
     symptoms(ID, Symptoms),
     random(X),
     (
-    (CurrentStage == asymptomatic, X < 0) -> advance_dissease_asymptomatic(CurrentStage, AgeGroup, NextStage);
-    (member(CurrentStage, [symptomatic, critical]), X < 1) -> advance_dissease(CurrentStage, AgeGroup, Symptoms, NextStage)
+    (CurrentStage == asymptomatic, X < 0.8) -> advance_dissease_asymptomatic(CurrentStage, AgeGroup, NextStage);
+    (member(CurrentStage, [symptomatic, critical]), X < 0.8) -> advance_dissease(CurrentStage, AgeGroup, Symptoms, NextStage);
+    NextStage = CurrentStage
     ).
 
 :-dynamic(symptoms/2).
